bb1d65c2bfc2456de6e11064701d266f936204ea
srikanta <srikanta.ydon@gmail.com>
2018-02-04 22:22:36 +0530

    These examples are from internet

---
+++ FirstSpringRoo/src/test/java/com/app/domain1/FirstClassDataOnDemand.java
@@ -0,0 +1,6 @@
+package com.app.domain1;
+import org.springframework.roo.addon.dod.RooDataOnDemand;
+
+@RooDataOnDemand(entity = FirstClass.class)
+public class FirstClassDataOnDemand {
+}
---
+++ FirstSpringRoo/src/test/java/com/app/domain1/FirstClassIntegrationTest.java
@@ -0,0 +1,11 @@
+package com.app.domain1;
+import org.junit.Test;
+import org.springframework.roo.addon.test.RooIntegrationTest;
+
+@RooIntegrationTest(entity = FirstClass.class)
+public class FirstClassIntegrationTest {
+
+    @Test
+    public void testMarkerMethod() {
+    }
+}
